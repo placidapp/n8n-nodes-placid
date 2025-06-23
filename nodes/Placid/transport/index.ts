ff354e6c929d97c8e3f3f5a542db5401ed9a46f3
Armin <armin@nifty.at>
2025-06-23 13:55:11 +0200

    quick cleanup

--- nodes/Placid/transport/index.ts
+++ nodes/Placid/transport/index.ts
@@ -1,22 +1,6 @@
-import { IExecuteFunctions, ILoadOptionsFunctions, IDataObject, IHttpRequestMethods } from 'n8n-workflow';
 import { PlacidConfig } from '../utils/config';
 
 const BASE_URL = 'https://api.placid.app/api/integromat';
-
-export async function placidApiRequest(
-	this: IExecuteFunctions | ILoadOptionsFunctions,
-	method: IHttpRequestMethods,
-	endpoint: string,
-	body?: IDataObject,
-): Promise<any> {
-	const options = {
-		method,
-		url: `${BASE_URL}${endpoint}`,
-		body,
-	};
-
-	return await this.helpers.httpRequestWithAuthentication.call(this, 'placidApi', options);
-}
 
 export const requestDefaults = {
 	baseURL: BASE_URL,
